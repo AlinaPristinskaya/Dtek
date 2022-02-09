8385807a66357fe6d02dcbb2bc2aacd194de4a15
AlinaPristinskaya <pristinskayaalina9@gmail.com>
2022-02-09 16:20:37 +0200

    burger settings

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
       <div class="header__logo">
         <img src="../assets/img/logo.svg" width="98" height="33" alt="ДТЭК" />
       </div>
+
       <div>
         <button
           type="button"
@@ -13,52 +14,52 @@
           aria-controls="menu-container"
         >
           <svg
-            width="40"
-            height="40"
+            width="20"
+            height="20"
             aria-label="Переключатель мобильного меню"
           >
             <use
               class="icon-cross"
-              href="../assets/img/spritecrossmenu.svg#cross"
+              href="../assets/img/spritecrossmenu.svg#settings"
             ></use>
             <use
               class="icon-menu"
-              href="../assets/img/spritecrossmenu.svg#menu"
+              href="../assets/img/spritecrossmenu.svg#settings"
             ></use>
           </svg>
         </button>
-      </div>
 
-      <div class="menu-container" id="menu-container" data-menu>
-        <div class="header__user">
-          <a href="#" class="svga">
-            <svg class="header__icon">
-              <use href="../assets/img/symbol-defs.svg#icon-user"></use></svg
-            ><span class="user">{{ userName }}</span></a
-          >
-        </div>
+        <div class="menu-container" id="menu-container" data-menu>
+          <div class="header__user">
+            <a href="#" class="svga">
+              <svg class="header__icon">
+                <use href="../assets/img/symbol-defs.svg#icon-user"></use></svg
+              ><span class="user">{{ userName }}</span></a
+            >
+          </div>
 
-        <div class="header__user">
-          <el-tooltip
-            class="item"
-            effect="dark"
-            content="Выход"
-            placement="top-start"
-          >
+          <div class="header__user">
+            <el-tooltip
+              class="item"
+              effect="light"
+              content="Выход"
+              placement="right-start"
+            >
+              <a href="#" class="svga"
+                ><svg @click="logout" class="header__icon">
+                  <use
+                    href="../assets/img/symbol-defs.svg#icon-exit"
+                  ></use></svg></a
+            ></el-tooltip>
+          </div>
+
+          <div class="header__user">
             <a href="#" class="svga"
-              ><svg @click="logout" class="header__icon">
-                <use
-                  href="../assets/img/symbol-defs.svg#icon-exit"
-                ></use></svg></a
-          ></el-tooltip>
-        </div>
-
-        <div class="header__user">
-          <a href="#" class="svga"
-            ><ul class="lang">
-              <li><span>UA</span></li>
-            </ul></a
-          >
+              ><ul class="lang">
+                <li><span>UA</span></li>
+              </ul></a
+            >
+          </div>
         </div>
       </div>
     </div>
@@ -123,6 +124,7 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+
     @media screen and (min-width: 768px) {
       display: flex;
       align-items: center;
@@ -146,6 +148,10 @@ export default {
     color: $--color-black;
     font-weight: 500;
     font-size: 18px;
+    @media screen and (max-width: 767px) {
+      display: block;
+      height: 30px;
+    }
   }
   &__content {
     font-size: 12px;
@@ -182,7 +188,7 @@ export default {
   }
 }
 .user {
-  @media (max-width: 768px) {
+  @media (max-width: 767px) {
     display: none;
   }
 }
@@ -195,6 +201,7 @@ export default {
   transition-property: color, border;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 250ms;
+
   &:hover,
   &:focus {
     color: $--color-primary;
@@ -209,13 +216,17 @@ export default {
     display: none;
     position: fixed;
     top: 100px;
-    right: 24px;
-
+    right: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    background-color: $--color-white;
+    border: 1px solid $--color-grey;
     z-index: 99;
-
     transform: translateX(-50%);
     transform: translateY(100%);
-    background-color: $--color-white;
+    :not(:last-child) {
+      margin-bottom: 5px;
+    }
     &.is-open {
       display: block;
       transform: translateY(0);
@@ -230,12 +241,6 @@ export default {
     align-items: center;
     :not(:last-child) {
       margin-right: 30px;
-    }
-  }
-  @media (max-width: 768px) {
-    :not(:last-child) {
-      margin-right: 10px;
-      font-size: 12px;
     }
   }
 }
